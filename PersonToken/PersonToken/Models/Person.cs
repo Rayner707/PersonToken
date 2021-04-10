948c2852d0545ff0e062268c8fc52a00942573fd
Rayner Jimmy Tapia Caballero <a2019116944@estudiantes.upsa.edu.bo>
2021-04-10 06:08:19 -0700

    Se adiciono el controlador People con token

---
+++ PersonToken/PersonToken/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonToken.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+
+        public int CovidCount { get; set; }
+
+
+    }
+}
