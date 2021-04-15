a55a2b249c0699d9e837739801ea101645057cdd
Rayner Jimmy Tapia Caballero <a2019116944@estudiantes.upsa.edu.bo>
2021-04-15 15:47:47 -0700

    Se agrego proyecto MVC  webpersonToken

---
+++ PersonToken/WebPersonToken/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebPersonToken.MvcApplication" Language="C#" %>
---
+++ PersonToken/WebPersonToken/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPersonToken.Startup))]
+namespace WebPersonToken
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
